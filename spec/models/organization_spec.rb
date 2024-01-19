b64cdd9dde96d02985cb41087e59f1edec7aec52
Liam Colburn <colburnw@oregonstate.edu>
2024-01-19 11:37:23 -0800

    Add agreement numbers attribute tests for Organization

--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -51,5 +51,35 @@ RSpec.describe Organization, type: :model do
     expect(organization).to respond_to(:transportation)
   end
 
-  # TODO: Add tests for attr_accesor
+  it "has an agreement_one" do
+    expect(organization).to respond_to(:agreement_one)
+  end
+
+  it "has an agreement_two" do
+    expect(organization).to respond_to(:agreement_two)
+  end
+
+  it "has an agreement_three" do
+    expect(organization).to respond_to(:agreement_three)
+  end
+
+  it "has an agreement_four" do
+    expect(organization).to respond_to(:agreement_four)
+  end
+
+  it "has an agreement_five" do
+    expect(organization).to respond_to(:agreement_five)
+  end
+
+  it "has an agreement_six" do
+    expect(organization).to respond_to(:agreement_six)
+  end
+
+  it "has an agreement_seven" do
+    expect(organization).to respond_to(:agreement_seven)
+  end
+
+  it "has an agreement_eight" do
+    expect(organization).to respond_to(:agreement_eight)
+  end
 end
