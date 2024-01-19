918ba4e50f36f3951bb0ffc7f02f837d936985fe
Liam Colburn <colburnw@oregonstate.edu>
2024-01-19 15:51:42 -0800

    Add tickets association test for Region

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -2,10 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Region, type: :model do
 
-  it "exists" do
-    Region.new
-  end
-
   it "has a name" do
     region = Region.new
     expect(region).to respond_to(:name)
@@ -15,6 +11,10 @@ RSpec.describe Region, type: :model do
     name = 'Mt. Hood'
     region = Region.new(name: name)
     result = region.to_s
+  end
+
+  it "has many tickets" do
+    should have_many(:tickets).class_name("Ticket")
   end
 
 end
