b35a9efb0ff353ad05c7c200cbabac5741ceb348
Liam Colburn <colburnw@oregonstate.edu>
2024-01-19 10:48:47 -0800

    Add descriptor and instantiation test for Organization

--- app/models/organization.rb
+++ app/models/organization.rb
@@ -1,3 +1,9 @@
+#/
+# Class Organization
+# Contains information on Organizations applied to or working with
+# the DSR
+# /
+
 class Organization < ApplicationRecord
 
   attr_accessor :agreement_one, :agreement_two, :agreement_three, :agreement_four, :agreement_five, :agreement_six, :agreement_seven, :agreement_eight
--- spec/models/organization_spec.rb
+++ spec/models/organization_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Organization, type: :model do
-
+  it 'exists' do
+    organization = Organization.new
+  end
 end
