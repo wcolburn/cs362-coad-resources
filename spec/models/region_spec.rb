75d90b7744ca0401f69ea896ac5dd7228b2a96b0
Liam Colburn <colburnw@oregonstate.edu>
2024-01-19 15:56:00 -0800

    Refactor region_spec to have a let statement

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -1,9 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Region, type: :model do
+  let(:region) { Region.new }
 
   it "has a name" do
-    region = Region.new
     expect(region).to respond_to(:name)
   end
 
