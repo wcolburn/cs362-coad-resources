3380886991a41ee1a25d720da0261b849a105d75
Liam Colburn <colburnw@oregonstate.edu>
2024-01-30 15:40:58 -0800

    Add test for validate name uniqueness

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -21,4 +21,8 @@ RSpec.describe Region, type: :model do
     should validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
   end
 
+  it "validates uniqueness of name" do
+    should validate_uniqueness_of(:name).case_insensitive
+  end
+
 end
