72e2daadbf87d60b3e040bf4d98a86cb0ecff45f
Liam Colburn <colburnw@oregonstate.edu>
2024-02-03 19:49:18 -0800

    Simplify region to_s test

--- spec/models/region_spec.rb
+++ spec/models/region_spec.rb
@@ -8,8 +8,6 @@ RSpec.describe Region, type: :model do
   end
 
   it "has a string representation that is its name" do
-    name = 'Mt. Hood'
-    region = Region.new(name: name)
     result = region.to_s
   end
 
