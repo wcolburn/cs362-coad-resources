03681f1922d93fc92a96d8bc3563fb2ca16d4258
Liam Colburn <colburnw@oregonstate.edu>
2024-02-04 16:59:19 -0800

    Refactor Ticket.closed test with factories

--- spec/factories/tickets.rb
+++ spec/factories/tickets.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :ticket do
     name
+    phone {"31301234123"}
   end
 end
--- spec/models/ticket_spec.rb
+++ spec/models/ticket_spec.rb
@@ -69,13 +69,13 @@ RSpec.describe Ticket, type: :model do
   end
 
   it "closed scope method returns all closed Tickets" do
-    r1 = Region.create!(name:"Bend")
-    rc1 = ResourceCategory.create!(name:"Food")
-    r2 = Region.create!(name:"Redmond")
-    rc2 = ResourceCategory.create!(name:"Water")
-    t1 = Ticket.create!(closed:true, name:"Closed Ticket", phone:"31301234123", region_id:r1.id, resource_category_id:rc1.id)
-    t2 = Ticket.create!(closed:false, name:"Open Ticket", phone:"31301234123", region_id:r2.id, resource_category_id:rc2.id)
-    expect(Ticket.closed.map(&:name)).to eq(["Closed Ticket"])
+    r1 = create(:region)
+    rc1 = create(:resource_category)
+    r2 = create(:region)
+    rc2 = create(:resource_category)
+    closed_ticket = create(:ticket, closed:true, region_id:r1.id, resource_category_id:rc1.id)
+    open_ticket = create(:ticket, closed:false, region_id:r2.id, resource_category_id:rc2.id)
+    expect(Ticket.closed).to eq([closed_ticket])
   end
 
   it "open scope method returns all opened Tickets" do
