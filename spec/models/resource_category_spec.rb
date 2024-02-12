f691bdf44a026115181b3548d4e2ee2dadf6cdd3
TParr4196 <tparr@LAPTOP-0O0FGHKA>
2024-02-11 17:19:11 -0800

    3.1 Tyler

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -23,4 +23,16 @@ RSpec.describe ResourceCategory, type: :model do
     it "Has and belongs to Organization" do
         should have_and_belong_to_many(:organizations).class_name("Organization")
     end
+
+    it "Validate presence of name" do
+        should validate_presence_of(:name)
+    end
+
+    it "Validates length of name is at least 1 and at most 255" do
+        should validate_length_of(:name).is_at_least(1).is_at_most(255).on(:create)
+    end
+    
+    it "Validates uniqueness of name" do
+        should validate_uniqueness_of(:name).case_insensitive
+    end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -19,4 +19,31 @@ RSpec.describe User, type: :model do
     it "Belongs to Organization" do
         should belong_to(:organization).class_name("Organization").optional
     end
+
+    it "Validate presence of email" do
+        should validate_presence_of(:email)
+    end
+
+    it "Validates length of email is at least 1 and at most 255" do
+        should validate_length_of(:email).is_at_least(1).is_at_most(255).on(:create)
+    end
+
+    it "validates format of email" do
+        should allow_values("email@provider.com").for(:email)
+        should_not allow_values("email@provider").for(:email)
+        should_not allow_values("email.com").for(:email)
+        should_not allow_values("@provider.com").for(:email)
+    end
+
+    it "Validates uniqueness of email" do
+        should validate_uniqueness_of(:email).case_insensitive
+    end
+
+    it "Validate presence of password" do
+        should validate_presence_of(:password)
+    end
+
+    it "Validates length of password is at least 7 and at most 255" do
+        should validate_length_of(:password).is_at_least(7).is_at_most(255).on(:create)
+    end
 end
