148e8163a89981c0e49ed5ed9c533fe1912b0c66
TParr4196 <tparr@LAPTOP-0O0FGHKA>
2024-02-11 16:01:43 -0800

    2.4 Tyler

--- spec/models/resource_category_spec.rb
+++ spec/models/resource_category_spec.rb
@@ -1,7 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe ResourceCategory, type: :model do
+    
     it "exists" do
         ResourceCategory.new
+    end
+
+    it "Has a name" do
+        resource = ResourceCategory.new
+        expect(resource).to respond_to(:name)
+    end
+
+    it "has an active" do
+        resource = ResourceCategory.new
+        expect(resource).to respond_to(:active)
     end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,18 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
     it "exists" do
         User.new
     end
 
+    it "Has an email" do
+        user = User.new
+        expect(user).to respond_to(:email)
+    end
+
+    it "Has a role" do
+        user = User.new
+        expect(user).to respond_to(:role)
+    end
 end
