5e30287d0038f0d42af5e75a86097b11de651c9c
Liam Colburn <colburnw@oregonstate.edu>
2024-02-13 18:21:44 -0800

    Add Ticket #new test

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
+  let(:ticket) { create(:ticket) }
+
+  describe 'GET #new' do
+    it { expect(get(:new)).to be_successful }
+  end
 
 end
