cd77cf05dce4e7b0a567dfe975edcf8b0dfcc4f3
Liam Colburn <colburnw@oregonstate.edu>
2024-02-13 18:28:10 -0800

    Change user factory to example from class

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,5 +1,22 @@
 FactoryBot.define do
-    factory :user do
-      email
+  factory :user do
+    email
+    password { "fake_password" }
+
+    before(:create) { |user| user.skip_confirmation! }
+
+    trait :organization_approved do
+      role { :organization }
+      organization_id { create(:organization, :approved).id }
+    end
+
+    trait :organization_unapproved do
+      role { :organization }
+      organization_id { create(:organization).id }
+    end
+
+    trait :admin do
+      role { :admin }
     end
   end
+end
