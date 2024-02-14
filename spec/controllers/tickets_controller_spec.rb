05ac8f29843baea5f95e34cc78156969c7f0b036
Liam Colburn <colburnw@oregonstate.edu>
2024-02-13 18:37:35 -0800

    Add Ticket test for #show for approved org user

--- spec/controllers/tickets_controller_spec.rb
+++ spec/controllers/tickets_controller_spec.rb
@@ -1,10 +1,21 @@
 require 'rails_helper'
 
 RSpec.describe TicketsController, type: :controller do
-  let(:ticket) { create(:ticket) }
+  let(:basic_ticket) { create(:ticket) }
+  let(:ticket) { create(:ticket, :region, :resource_category) }
+  let(:organization_approved) { create(:user, :organization_approved) }
+  let(:organization_unapproved) { create(:user, :organization_unapproved) }
 
   describe 'GET #new' do
     it { expect(get(:new)).to be_successful }
+  end
+
+  describe 'GET #show for approved organization user' do
+    before do
+      sign_in organization_approved
+    end
+
+    it { expect(get(:show, params: { id: ticket.id })).to be_successful }
   end
 
 end
--- spec/factories/tickets.rb
+++ spec/factories/tickets.rb
@@ -2,5 +2,14 @@ FactoryBot.define do
   factory :ticket do
     name
     phone {"31301234123"}
+
+    trait :region do
+      region_id { create(:region).id }
+    end
+
+    trait :resource_category do
+      resource_category_id { create(:resource_category).id }
+    end
+
   end
 end
