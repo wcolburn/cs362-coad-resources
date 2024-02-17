6ca6b3e8567a1195505d74e4c3f5a35d1cb915c6
Liam Colburn <colburnw@oregonstate.edu>
2024-02-16 19:20:13 -0800

    Add get index tests for region controller

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe RegionsController, type: :controller do
+  let(:admin) { create(:user, :admin) }
+  let(:user) { create(:user, :organization_approved) }
+
+  describe "GET #index" do
+    it "get as admin" do
+      sign_in admin
+      expect(get(:index)).to be_successful
+    end
+
+    it "cannot get as regular user" do
+      sign_in user
+      expect(get(:index)).to redirect_to(dashboard_path)
+    end
+  end
 
 end
