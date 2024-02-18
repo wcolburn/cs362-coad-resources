5fe7a6f4f62aadea41263d8b3f28005417fa2fa2
Liam Colburn <colburnw@oregonstate.edu>
2024-02-17 22:03:16 -0800

    Add get #show test for region controller

--- spec/controllers/regions_controller_spec.rb
+++ spec/controllers/regions_controller_spec.rb
@@ -1,8 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe RegionsController, type: :controller do
+  let(:region) { create(:region) }
   let(:admin) { create(:user, :admin) }
-  let(:user) { create(:user, :organization_approved) }
+  let(:organization_approved) { create(:user, :organization_approved) }
+  let(:organization_unapproved) { create(:user, :organization_unapproved) }
 
   describe "GET #index" do
     it "get as admin" do
@@ -11,7 +13,7 @@ RSpec.describe RegionsController, type: :controller do
     end
 
     it "cannot get as regular user" do
-      sign_in user
+      sign_in organization_approved
       expect(get(:index)).to redirect_to(dashboard_path)
     end
   end
@@ -23,4 +25,14 @@ RSpec.describe RegionsController, type: :controller do
     end
   end
 
+  describe 'GET #show for admin user succeeds' do
+    it "succeeds for admin" do
+      sign_in admin
+      expect(get(:show, params: { id: region.id })).to be_successful
+    end
+    it "fails for regular user" do
+      sign_in organization_approved
+      expect(get(:show, params: { id: region.id })).to_not be_successful
+    end
+  end
 end
