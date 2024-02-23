be6f42075dd30047c070390c7fdd580c1875e87a
Liam Colburn <colburnw@oregonstate.edu>
2024-02-22 17:35:22 -0800

    Add tests for application helper

--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -2,4 +2,12 @@ require 'rails_helper'
 
 RSpec.describe ApplicationHelper, type: :helper do
 
+  describe "test full_title" do
+    it { expect(helper.full_title()).to eq 'Disaster Resource Network' }
+  end
+
+  describe "test full_title with specific page title" do
+    it { expect(helper.full_title('Central Oregon')).to eq 'Central Oregon | Disaster Resource Network' }
+  end
+
 end
