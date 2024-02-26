70ca3ffa21fe92aecf1353c9c98c8d2d8557cca6
Liam Colburn <colburnw@oregonstate.edu>
2024-02-26 15:03:40 -0800

    Add admin deletes ticket at dashboard test

--- spec/features/tickets/admin_deletes_ticket_spec.rb
+++ spec/features/tickets/admin_deletes_ticket_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Ticket', type: :feature do
+  before do
+    @ticket = create(:ticket, :region, :resource_category)
+    @admin = create(:user, :admin)
+  end
 
+  it 'can be deleted by admin at dashboard' do
+    log_in_as @admin
+    visit dashboard_path
+    click_on @ticket.name
+    click_on 'Delete'
+    expect(current_path).to eq dashboard_path
+    expect(page.body).to_not have_text(@ticket.name)
+  end
 end
