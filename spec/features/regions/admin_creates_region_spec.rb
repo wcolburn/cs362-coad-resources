75d165e6c75233f0f48cbc7cb475f58e766c9a1e
Tyler <tplaxstar@gmail.com>
2024-02-26 14:46:41 -0800

    admin creates region spec

--- spec/features/regions/admin_creates_region_spec.rb
+++ spec/features/regions/admin_creates_region_spec.rb
@@ -2,4 +2,14 @@ require 'rails_helper'
 
 RSpec.describe 'Creating a Region', type: :feature do
 
+    it 'creates region as admin' do
+        admin=create(:user, :admin)
+        log_in_as(admin)
+        visit regions_path
+        click_on 'Add Region'
+        fill_in 'Name', with: 'Test Region'
+        click_on 'Add Region'
+        expect(current_path).to eq(regions_path)
+        expect(page.body).to have_text('Test Region')
+    end
 end
