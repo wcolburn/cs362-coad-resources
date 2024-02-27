7775a2fe83194c069ad6944dabe12b6dce8cdcf7
Liam Colburn <colburnw@oregonstate.edu>
2024-02-26 19:04:57 -0800

    Add feature test for release ticket

--- spec/features/tickets/release_ticket_spec.rb
+++ spec/features/tickets/release_ticket_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Releasing a ticket by an', type: :feature do
-
+  it 'approved organization' do
+    @user = create(:user, :organization_approved)
+    @ticket = create(:ticket, :region, :resource_category, organization: @user.organization)
+    log_in_as @user
+    visit dashboard_path
+    click_on 'Tickets'
+    click_on @ticket.name
+    click_on 'Release'
+    expect(current_path).to eq dashboard_path
+  end
 end
