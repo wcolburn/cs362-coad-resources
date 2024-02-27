f09fc2b7616661f57177a34aa49de9bd36ce60b4
Liam Colburn <colburnw@oregonstate.edu>
2024-02-26 18:53:46 -0800

    Add test for capture ticket feature

--- spec/features/tickets/capture_ticket_spec.rb
+++ spec/features/tickets/capture_ticket_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Capturing a ticket', type: :feature do
-
+  it 'approved organization user captures ticket from dashboard' do
+    @user = create(:user, :organization_approved)
+    @ticket = create(:ticket, :region, :resource_category)
+    log_in_as @user
+    visit dashboard_path
+    click_on 'Tickets'
+    click_on @ticket.name
+    click_on 'Capture'
+    expect(current_path).to eq dashboard_path
+  end
 end
