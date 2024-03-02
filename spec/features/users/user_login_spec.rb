4cfddc6bae44ae8d938d00230a83ac5e0b55bae2
Liam Colburn <colburnw@oregonstate.edu>
2024-03-01 16:24:57 -0800

    Add user login functionality test

--- spec/features/users/user_login_spec.rb
+++ spec/features/users/user_login_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe 'Logging in', type: :feature do
-
+  it 'logs in from dashboard' do
+    @user = create(:user, password: "testpassword")
+    visit dashboard_path
+    click_on 'Log in'
+    fill_in "Email address", with: @user.email
+    fill_in "Password", with: "testpassword"
+    click_on 'Sign in'
+    expect(current_path).to eq dashboard_path
+  end
 end
--- spec/features/users/user_registration_spec.rb
+++ spec/features/users/user_registration_spec.rb
@@ -1,5 +1,5 @@
 require 'rails_helper'
 
 RSpec.describe 'User registration', type: :feature do
-
+  # Instructor says this is not required since there is a Captcha issue
 end
