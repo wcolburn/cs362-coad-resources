cc6653245988de52ddfaac9a2666122473aa94f1
Tyler <tplaxstar@gmail.com>
2024-03-03 16:13:30 -0800

    deleting resource category

--- spec/features/resource_categories/admin_deleted_resource_category_spec.rb
+++ spec/features/resource_categories/admin_deleted_resource_category_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
 
 RSpec.describe 'Deleting a Resource Category', type: :feature do
-
+    it 'as admin' do
+        admin=create(:user, :admin)
+        log_in_as(admin)
+        resource_category=create(:resource_category, name:'Test')
+        visit resource_categories_path
+        click_on 'Test'
+        click_on 'Delete'
+        expect(current_path).to eq(resource_categories_path)
+        expect(current_path).to_not have_text('Test')
+    end
 end
